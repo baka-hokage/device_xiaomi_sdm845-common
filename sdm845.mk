180070d43a50816a732098692e4858ef86beb7f2
Bruno Martins <bgcngm@gmail.com>
2022-09-30 21:30:46 +0100

    sdm845-common: Remove dead targets
    Change-Id: I496e37e3d813a96ca3a0138dbc61cd1a90655244

--- sdm845.mk
+++ sdm845.mk
@@ -103,7 +103,6 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth.audio@2.1-impl:32 \
     audio.bluetooth.default \
-    libbthost_if \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor
 
@@ -152,7 +151,6 @@ PRODUCT_PACKAGES += \
     libdisplayconfig.qti \
     libqdMetaData \
     libtinyxml \
-    libtinyxml.vendor \
     memtrack.sdm845 \
     vendor.display.config@1.0.vendor \
     vendor.display.config@2.0 \
@@ -323,7 +321,6 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
     libjson \
-    libjson.vendor \
     librmnetctl
 
 # Recovery
