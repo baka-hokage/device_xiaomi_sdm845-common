d7ba64490f2713f8e1c48db5286ffe352645f7e8
baka-hokage <ayushbehera007@gmail.com>
2022-12-05 22:21:44 +0530

    sdm845-common: Nuke CameraGo

--- sdm845.mk
+++ sdm845.mk
@@ -115,8 +115,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     libdng_sdk.vendor \
-    vendor.qti.hardware.camera.device@1.0.vendor \
-    CameraGo
+    Snap \
+    vendor.qti.hardware.camera.device@1.0.vendor
 
 # Common init scripts
 PRODUCT_PACKAGES += \
